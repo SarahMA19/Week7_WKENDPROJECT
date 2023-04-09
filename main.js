8b591cb3e9a0efcfa14629694cbe6cf037b7fcdb
SarahMA19 <sarahgreisen@gmail.com>
2023-04-08 19:55:06 -0700

    round 12949375

--- main.js
+++ main.js
@@ -16,12 +16,17 @@ let weather = {
         const { icon, description } = data.weather[0];
         const { temp, humidity } = data.main;
         const { speed } = data.wind;
+        const { sunrise, sunset} = data.sys 
         document.querySelector(".city").innerText = "Weather in " + name;
         document.querySelector(".icon").src = "https://openweathermap.org/img/wn/" + icon + ".png"
         document.querySelector(".description").innerText = description;
-        document.querySelector(".temp").innerHTML = temp + " °F";
+        document.querySelector(".temp").innerHTML = (Math.round(temp)) + " °F";
         document.querySelector(".humidity").innerText = "Humidity: " + humidity + "%";
-        document.querySelector(".wind").innerText = "Wind Speed: " + speed + " mph";
+        document.querySelector(".wind").innerText = "Wind Speed: " + (Math.round(speed)) + " mph";
+        document.querySelector(".sunrise").innerHTML = "Sunrise: " + moment(sunrise * 1000).format('h:mm a');
+        document.querySelector(".sunset").innerHTML = "Sunset: " + moment(sunset * 1000).format('h:mm a');
+        document.querySelector(".weather").classList.remove("loading");
+        document.body.style.backgroundImage = "url('https://source.unsplash.com/1600x900/?" + name + "')"
     },
     search: function () {
         this.fetchWeather(document.querySelector(".search-bar").value);
@@ -37,3 +42,6 @@ document.querySelector(".search-bar").addEventListener("keyup", function (event)
         weather.search();
     }
 })
+
+weather.fetchWeather("Seattle");
+
